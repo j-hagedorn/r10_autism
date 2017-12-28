e3a2f36c58de9cf08eb9f57310ac3ef5cfa40014
Joshh <josh.hagedorn@gmail.com>
2017-12-28 08:36:20 -0500

    Point to updated function

--- make_autism_reports.R
+++ make_autism_reports.R
@@ -7,77 +7,7 @@ library(tidyverse); library(magrittr); library(stringr);library(lubridate);libra
 # Combine datasets as files to be read by subsequent .Rmd files 
 # (to avoid running multiple times)
 
-  # Read in .csv files as dataframes
-  csv_path <- "C:/Users/joshh/OneDrive - TBD Solutions LLC/files/Region10/Autism/"
-  genesee <- read_csv(paste0(csv_path,"Autism.Genesse.csv"), skip = 7)
-  lapeer <- read_csv(paste0(csv_path,"Autism.Lapeer.csv"), skip = 7)
-  sanilac <- read_csv(paste0(csv_path,"Autism.Sanilac.csv"), skip = 7)
-  stclair <- read_csv(paste0(csv_path,"Autism.StClair.csv"), skip = 7)
-  # Bind separate CMH dataframes together
-  svs <- genesee %>% bind_rows(lapeer,sanilac,stclair)
-  rm(genesee);rm(lapeer);rm(sanilac);rm(stclair)
-  # Remove cols where all values are NA
-  svs <- Filter(function(x)!all(is.na(x)), svs)
-  
-  # Clean colnames (rm spaces, other symbols, add underscore sep)
-  names(svs) <- gsub(":", "", names(svs))
-  names(svs) <- gsub(" |-", "_", names(svs))
-  
-  # Clean data to prepare for analysis
-  svs %<>%
-    # Clean Medicaid ID field
-    mutate(
-      # Trim lead / trail whitespace
-      MEDICAID_ID = str_trim(MEDICAID_ID),
-      # Remove alpha and special chars
-      MEDICAID_ID = str_replace_all(MEDICAID_ID, "[[:alpha:]]", ""),
-      MEDICAID_ID = str_replace_all(MEDICAID_ID, "[[:punct:]]", ""),
-      # Convert blanks to NA
-      MEDICAID_ID = ifelse(MEDICAID_ID == "", yes = NA, no = MEDICAID_ID), 
-      # If string > 10 chars, include only last 10 chars
-      MEDICAID_ID = ifelse(nchar(as.character(MEDICAID_ID)) > 10,
-                           yes = substr(MEDICAID_ID, 
-                                        start = nchar(as.character(MEDICAID_ID)) - 9, 
-                                        stop = nchar(as.character(MEDICAID_ID))),
-                           no = MEDICAID_ID),
-      # If string < 10 chars, pad with leading zeroes
-      MEDICAID_ID = ifelse(nchar(as.character(MEDICAID_ID)) < 10,
-                           yes = sprintf("%010d", as.integer(MEDICAID_ID)),
-                           no = MEDICAID_ID),
-      # Make 'NA' & 0000000000 to NA
-      MEDICAID_ID = ifelse(MEDICAID_ID %in% c("        NA","NA","0000000000"), 
-                           yes = NA,
-                           no = MEDICAID_ID),
-      # Convert to factor
-      MEDICAID_ID = as.factor(MEDICAID_ID)
-    ) %>%
-    # Change numeric ID vars to characters
-    mutate_at(
-      .vars = vars(PRV_ID,CON_ID,CLM_ID:PRIM_INS_ID),
-      .funs = funs(as.character)
-    ) %>%
-    # Change all character columns to factors
-    mutate_if(is.character,as.factor) %>%
-    # Clean date fields to prepare for analysis
-    mutate(
-      FROM_DATE = mdy(FROM_DATE),
-      THRU_DATE = mdy(THRU_DATE)
-    ) %>%
-    # Transform Y/N responses into logical vars
-    mutate_at(
-      .vars = vars(HAB_WAIVER:HMP_BUCKET),
-      .funs = funs(. == "Y")
-    ) %>%
-    # Recode CMH names for consistent reference 
-    mutate(
-      PROVIDER_NAME = recode(
-        PROVIDER_NAME,
-        `6-Genesee Health System` = "Genesee Health System",
-        `2-Lapeer County Community Mental Health` = "Lapeer County CMH",
-        `3-St. Clair County Community Mental Health` = "St. Clair County CMH",
-        `4-Sanilac County Community Mental Health` = "Sanilac County CMH"
-      )
-    )
+  source("prep/read_svs.R")
   
   # Write to file
   # Note that this will overwrite any previous files with the same name
